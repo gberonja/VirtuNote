bc80b3903c7bcf86356e9b975a691ff4700fb1ee
Gaketz <gabriel.beronja@gmail.com>
2024-12-26 12:55:05 +0100

    Dodan osnovni FastAPI server s endpointom za status i Pydantic modelima

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+from app.models import Note
 
-app = FastAPI()
+app = FastAPI(title="VirtuNote API", description="API za pohranu i razmjenu notnih zapisa", version="1.0.0")
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Welcome to VirtuNote API!"}
 
 @app.get("/status")
 def read_status():
-    return {"status": "Radi API!"}
+    return {"status": "API is running!"}
